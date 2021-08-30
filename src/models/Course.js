424d62b72ae5c4279a357e270cd22463cc92303c
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-08-30 20:29:54 -0300

    update models

--- src/models/Course.js
+++ src/models/Course.js
@@ -1,14 +1,39 @@
 const mongoose = require('mongoose')
 
-const schema = mongoose.Schema({
-  title: String,
-  thumb: String,
-  school: String,
-  description: String,
-  year: String,
-  testimonial: String,
-  link: String,
-  technologies: Array,
-})
+const CourseSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Please provide title'],
+    },
+    thumb: {
+      type: String,
+    },
+    school: {
+      type: String,
+      required: [true, 'Please provide school'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide description'],
+    },
+    year: {
+      type: String,
+      required: [true, 'Please provide year'],
+    },
+    testimonial: {
+      type: String,
+      required: [true, 'Please provide testimonial'],
+    },
+    url: {
+      type: String,
+      required: [true, 'Please provide link'],
+    },
+    technologies: {
+      type: Array,
+    },
+  },
+  { timestamps: true },
+)
 
-module.exports = mongoose.model('Course', schema)
+module.exports = mongoose.model('Course', CourseSchema)
